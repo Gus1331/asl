eae5e0c1f554239521f68cf723633a6908767f4c
Kaique-Gmes <kaique.sgomes@sptech.school>
2023-12-05 19:11:59 -0300

    Transformando dados Dashboard Produção e Renda

--- site-institucional/src/controllers/medidaController.js
+++ site-institucional/src/controllers/medidaController.js
@@ -35,8 +35,23 @@ function buscarMedidasEmTempoReal(req, res) {
     });
 }
 
+function buscarDadosDiario(req, res) {
+
+    medidaModel.buscarDadosDiario().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
-
+    buscarMedidasEmTempoReal,
+    buscarDadosDiario,
 }
--- site-institucional/src/routes/medidas.js
+++ site-institucional/src/routes/medidas.js
@@ -11,4 +11,8 @@ router.get("/tempo-real/:idAquario", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
 
+router.get("/diario", function (req, res) {
+    medidaController.buscarDadosDiario(req, res);
+});
+
 module.exports = router;
