7ebe55d1d56e3d152c3c36f51964d427b77ab0b7
Felipe Conde <felipe.conde@sptech.school>
2023-12-06 00:21:49 -0300

    controller criado para levar a rota os dados

--- site-institucional/src/controllers/avisoController.js
+++ site-institucional/src/controllers/avisoController.js
@@ -126,11 +126,27 @@ function deletar(req, res) {
         );
 }
 
+function buscarUltimasMedidas(res) {
+
+    avisoModel.buscarUltimasMedidas().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+
 module.exports = {
     listar,
     listarPorUsuario,
     pesquisarDescricao,
     publicar,
     editar,
-    deletar
+    deletar,
+    buscarUltimasMedidas
 }
